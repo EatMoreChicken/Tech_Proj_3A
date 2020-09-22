f4c4f7218780735bb13d048dc212b24449cd0bc4
Varghese Charly <vcharly@usf.edu>
2020-09-22 14:04:55 -0400

    First commit with author information and one method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿/*
+Author: Varghese Charly
+Date: 09/22/2020
+Purpose: The purpose of this project is to practice using method.
+The code will ask the user to enter thier name and then say hello to the name that they entered.
+*/
+
+using System;
 
 namespace Tech_Proj_3A
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                UserPrompt();
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void UserPrompt()
+        {
+            Console.WriteLine("What is your name?");
         }
     }
 }
