038b4267b311dc773b8a7eae46f9faf8fe59aa2a
Varghese Charly <vcharly@usf.edu>
2020-09-22 12:55:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Proj_3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
